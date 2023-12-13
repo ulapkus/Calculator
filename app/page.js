339df37bdad1e0862a949bc62667bbaef726cb7c
ulapkus <ulapkus@gmail.com>
2023-12-13 16:50:26 +0800

    imported completed calculator

--- app/page.js
+++ app/page.js
@@ -1,113 +1,119 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import React, { useState } from "react";
+
+function Calculator() {
+  const [count, setCount] = React.useState("0");
+
+  const buttonArr = [
+    { number: "zero", text: "0" },
+    { number: "one", text: "1" },
+    { number: "two", text: "2" },
+    { number: "three", text: "3" },
+    { number: "four", text: "4" },
+    { number: "five", text: "5" },
+    { number: "six", text: "6" },
+    { number: "seven", text: "7" },
+    { number: "eight", text: "8" },
+    { number: "nine", text: "9" },
+    { number: "subtract", text: "-" },
+    { number: "add", text: "+" },
+    { number: "multiply", text: "*" },
+    { number: "divide", text: "/" },
+    { number: "decimal", text: "." },
+  ];
+
+  const buttonPress = (event) => {
+    buttonArr.forEach((item) => {
+      if (event.target.id === "zero" && count === "0") {
+        return;
+      } else if (event.target.id === "add" && count.match(/[-+*/]+$/)) {
+        setCount(count.slice(0, -count.match(/[-+*/]+$/)[0].length) + "+");
+      } else if (event.target.id === "add" && count.match(/[-+*/]$/)) {
+        setCount(count.slice(0, -1) + "+");
+      } else if (event.target.id === "multiply" && count.match(/[+*/]$/)) {
+        setCount(count.slice(0, -1) + "*");
+      } else if (event.target.id === "divide" && count.match(/[+*/]$/)) {
+        setCount(count.slice(0, -1) + "/");
+      } else if (event.target.id === item.number && count === "0") {
+        setCount(item.text);
+      } else if (event.target.id === "zero" && count.match(/[-+*/]*0{1,}/)) {
+        return;
+      } else if (
+        event.target.id === "decimal" &&
+        (count.match(/\d+\.$/) || count.match(/\d+\.\d+$/))
+      ) {
+        setCount(count);
+      } else if (event.target.id === item.number) {
+        setCount(count + "" + item.text);
+      } else if (event.target.id === "clear") {
+        setCount("0");
+      } else if (event.target.id === "equals") {
+        setCount(eval(count).toString());
+      }
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <div id="container">
+    <main>
+      <section id="display">{count}</section>
+<section id="button-box">
+      <button className="button" id="seven" onClick={buttonPress}>
+        7
+      </button>
+      <button className="button" id="eight" onClick={buttonPress}>
+        8
+      </button>
+      <button className="button" id="nine" onClick={buttonPress}>
+        9
+      </button>
+      <button className="button" id="subtract" onClick={buttonPress}>
+        -
+      </button>
+      <button className="button" id="four" onClick={buttonPress}>
+        4
+      </button>
+      <button className="button" id="five" onClick={buttonPress}>
+        5
+      </button>
+      <button className="button" id="six" onClick={buttonPress}>
+        6
+      </button>
+      <button className="button" id="add" onClick={buttonPress}>
+        +
+      </button>
+      <button className="button" id="one" onClick={buttonPress}>
+        1
+      </button>
+      <button className="button" id="two" onClick={buttonPress}>
+        2
+      </button>
+      <button className="button" id="three" onClick={buttonPress}>
+        3
+      </button>
+      <button className="button" id="multiply" onClick={buttonPress}>
+        x
+      </button>
+      <button className="button" id="zero" onClick={buttonPress}>
+        0
+      </button>
+      <button className="button" id="decimal" onClick={buttonPress}>
+        .
+      </button>
+      <button className="button" id="divide" onClick={buttonPress}>
+        /
+      </button>
+      <button className="button" id="equals" onClick={buttonPress}>
+        =
+      </button>
+      <button className="button" id="clear" onClick={buttonPress}>
+        AC
+      </button>
+      </section>
     </main>
-  )
+    </div>
+  );
 }
+
+export default Calculator;
